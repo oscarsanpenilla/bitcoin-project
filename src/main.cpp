db110428a64c6ee740050bacb89e7edf94ea0c2b
Oscar Sandoval  Penilla <oscarsanpenilla@gmail.com>
2021-03-21 23:19:46 -0700

    Fetch Json Bitcoin Data in main

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,23 @@
 
 
 using namespace std;
+using namespace nlohmann;
 
 int main(int argc, char const *argv[])
 {
-    cout << "Bitcoin Project" << '\n';
-    Bitcoin bitcoin;
-    bitcoin.fetchBitcoinData();
 
+    cout << "Bitcoin Project" << '\n';
+
+    try {
+        Bitcoin bitcoin;
+        json bitcoinJsonData = bitcoin.fetchJsonBitcoinData();
+        cout << "1 BTC = " << "\n";
+        for (auto it = bitcoinJsonData.begin(); it != bitcoinJsonData.end(); ++it){
+            printf("\t(%3s)%10d %s\n", it.key().c_str(), it.value()["last"].get<int>(),
+                    it.value()["symbol"].get<std::string>().c_str());
+        }
+    }  catch (...) {
+        cerr << "Failed to Fetch bitcoin exchange rates" << '\n';
+    }
     return 0;
 }
