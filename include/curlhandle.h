6cfce6ba82e494c1d9a4789add1cf6be69b0b120
Oscar Sandoval  Penilla <oscarsanpenilla@gmail.com>
2021-03-21 23:15:21 -0700

    Use custom write data function instead of the default cout

--- include/curlhandle.h
+++ include/curlhandle.h
@@ -4,22 +4,30 @@
 #include <memory>
 #include <functional>
 #include "curl/curl.h"
+#include <iostream>
+
+typedef std::unique_ptr<CURL, std::function<void(CURL *)>> CURL_ptr;
 
 class CurlHandle
 {
-typedef std::unique_ptr<CURL, std::function<void(CURL *)>> CURL_ptr;
-private:
-    CURL_ptr curlPtr;
-    constexpr static auto deleter = [](CURL *c) {
-        curl_easy_cleanup(c);
-        curl_global_cleanup();
-    };
 
 public:
     CurlHandle();
 
     void setUrl(std::string url);
+    std::string getFetchData();
     CURLcode fetch();
+
+
+private:
+    std::string m_fetchData;
+    CURL_ptr curlPtr;
+    constexpr static auto deleter = [](CURL *c) {
+        curl_easy_cleanup(c);
+        curl_global_cleanup();
+    };
+    static size_t dataHandler(const char* buffer, std::size_t size,
+                              std::size_t nmemb, std::string* userData);
 };
 
 #endif //CURLHANDLE_H
--- src/curlhandle.cpp
+++ src/curlhandle.cpp
@@ -1,9 +1,10 @@
 #include "curlhandle.h"
 
-
 CurlHandle::CurlHandle() : curlPtr(curl_easy_init(), deleter)
 {
     curl_global_init(CURL_GLOBAL_ALL);
+    curl_easy_setopt(curlPtr.get(), CURLOPT_WRITEFUNCTION, dataHandler);
+    curl_easy_setopt(curlPtr.get(), CURLOPT_WRITEDATA, &m_fetchData);
 }
 
 void CurlHandle::setUrl(std::string url)
@@ -11,7 +12,27 @@ void CurlHandle::setUrl(std::string url)
     curl_easy_setopt(curlPtr.get(), CURLOPT_URL, url.c_str());
 }
 
+std::string CurlHandle::getFetchData()
+{
+    return m_fetchData;
+}
+
 CURLcode CurlHandle::fetch()
 {
     return curl_easy_perform(curlPtr.get());
 }
+
+extern "C" std::size_t CurlHandle::dataHandler(const char* buffer, std::size_t size,
+                                               std::size_t nmemb, std::string* userData)
+{
+    if (!userData){
+        std::cout << "UserData nullptr";
+        return 0;
+    }
+
+    userData->append(buffer, size*nmemb);
+//    std::cout << "userData " << *userData << "\n";
+    return size * nmemb;
+}
+
+
